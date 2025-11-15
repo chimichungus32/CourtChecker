a20b5cb413d5e61ac64f9beddfa205020fcc0afb
Leon <111322217+chimichungus32@users.noreply.github.com>
2025-11-15 11:49:12 +1100

    api doesnt return names anymore

--- backend/booking.py
+++ backend/booking.py
@@ -17,12 +17,10 @@ def get_booking_data(court_id: int, date: date):
   # booking data is a list
   for booking in booking_data:
     cleaned_booking = {}
-    if booking["Given_Names"] != "" and booking["Family_Name"] != "":
+    if "court" in booking["Name"].lower():
       cleaned_booking["Start_Date"] = booking["Start_Date"].lower()
       cleaned_booking["End_Date"] = booking["End_Date"].lower()
       cleaned_booking["Name"] = booking["Name"].lower()
-      cleaned_booking["First_Name"] = booking["Given_Names"]
-      cleaned_booking["Last_Name"] = booking["Family_Name"]
       cleaned_booking_data.append(cleaned_booking)
         
   sorted_data = sorted(cleaned_booking_data, key=lambda x: (x['Name'], x['Start_Date']))
@@ -45,34 +43,18 @@ def get_booking_data(court_id: int, date: date):
 
 #   cleaned_booking_data = []
 
-#   included_names = ["acrylic hard court", "synthetic grass court"]
-
-#   banned_booking_titles = ["Closed - Council Use", "Closed - Other"]
-
 #   # booking is dictionary
 #   # booking data is a list
 #   for booking in booking_data:
-#     if booking["PreventBooking"]:
-#       continue  
 #     cleaned_booking = {}
-#     for included_name in included_names:
-#       if included_name in booking["Name"].lower():
-#         cleaned_booking["Start_Date"] = booking["Start_Date"].lower()
-#         cleaned_booking["End_Date"] = booking["End_Date"].lower()
-#         cleaned_booking["Name"] = booking["Name"].lower()
-#         cleaned_booking["First_Name"] = booking["Given_Names"]
-#         cleaned_booking["Last_Name"] = booking["Family_Name"]
-      
-#         # Add acrylic hard court 20 bookings only if the venue is canoon (id 43)
-#         if cleaned_booking["Name"] == "acrylic hard court 20":
-#           if court_id == 43:
-#             cleaned_booking_data.append(cleaned_booking)
-#         else:
-#           cleaned_booking_data.append(cleaned_booking)
-
-#         print(cleaned_booking)
+#     if booking["Given_Names"] != "" and booking["Family_Name"] != "":
+#       cleaned_booking["Start_Date"] = booking["Start_Date"].lower()
+#       cleaned_booking["End_Date"] = booking["End_Date"].lower()
+#       cleaned_booking["Name"] = booking["Name"].lower()
+#       cleaned_booking["First_Name"] = booking["Given_Names"]
+#       cleaned_booking["Last_Name"] = booking["Family_Name"]
+#       cleaned_booking_data.append(cleaned_booking)
         
 #   sorted_data = sorted(cleaned_booking_data, key=lambda x: (x['Name'], x['Start_Date']))
-#   # print(sorted_data)
 
 #   return sorted_data
--- backend/main.py
+++ backend/main.py
@@ -23,13 +23,45 @@ app.add_middleware(
 @app.get("/booking/{court_id}")
 def return_booking_data(court_id: int, date: date): # Date is a query parameter
   booking_data = get_booking_data(court_id, date)
-  for booking in booking_data:
-    booking.pop("First_Name")
-    booking.pop("Last_Name")
   return booking_data # Fast api automatically returns a dictionary as json
 
-# Returns courts and names
-@app.get("/booking/names/{court_id}")
-def return_booking_data(court_id: int, date: date):
-  booking_data = get_booking_data(court_id, date)
-  return booking_data 
+
+
+# Api doesn't return names anymore
+
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from datetime import date
+# from booking import get_booking_data
+
+# app = FastAPI()
+
+# origins = [
+#     "http://172.31.96.178:3000",
+#     "https://court-checker.pages.dev",
+#     "https://court-checker-2.pages.dev",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+# # http://127.0.0.1:8000/booking/52?date=2025-04-06 example
+# # Returns courts only
+# @app.get("/booking/{court_id}")
+# def return_booking_data(court_id: int, date: date): # Date is a query parameter
+#   booking_data = get_booking_data(court_id, date)
+#   for booking in booking_data:
+#     booking.pop("First_Name")
+#     booking.pop("Last_Name")
+#   return booking_data # Fast api automatically returns a dictionary as json
+
+# # Api doesn't return names anymore
+# # Returns courts and names
+# @app.get("/booking/names/{court_id}")
+# def return_booking_data(court_id: int, date: date):
+#   booking_data = get_booking_data(court_id, date)
+#   return booking_data 
